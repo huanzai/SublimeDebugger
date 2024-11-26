6995827a96c0091f7e8627ebcca27e8574ac931b
huanzai <857763401@qq.com>
2024-11-26 23:14:44 +0800

    修复 lua 无法显示 table 数组内容

--- examples/mylua/main.lua
+++ examples/mylua/main.lua
@@ -3,6 +3,24 @@ require "debugger":start "127.0.0.1:12306":setup_patch():event "wait"
 local a = 1
 local b = 2
 
+local t = {
+    {a = 1, b = 2},
+    {a = 1, b = 2},
+    {a = 1, b = 2},
+    {a = 1, b = 2},
+
+    aa = 3,
+    bb = 4,
+    cc = {
+        a=1,b=3
+    },
+}
+
+local t2 = {
+    a = {11,22},
+    b = {11,22},
+}
+
 local function foo()
     return a + b
 end
